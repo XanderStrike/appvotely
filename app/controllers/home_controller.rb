5c1823fddd305365a20c429a6ba48a79e954c4c8
Alexander Standke <xanderstrike@gmail.com>
2017-07-27 14:37:38 -0700

    prevent duplicate voting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @survey = Survey.first
+
+    if cookies[:answered].to_i == @survey.id
+      redirect_to controller: 'home', action: 'results', survey_id: @survey.id
+    end
   end
 
   def answer
@@ -12,6 +16,7 @@ class HomeController < ApplicationController
   end
 
   def results
+    cookies.permanent[:answered] = params[:survey_id]
     @survey = Survey.find(params[:survey_id])
   end
 end
